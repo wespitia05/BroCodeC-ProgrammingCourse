300f3c8a0340f97a1d8872ee1fafb7a2f2656795
william <espiw@farmingdale.edu>
2026-03-21 16:54:32 -0400

    declaring data type integer and displaying value

--- variables_basic_data_types.cpp
+++ variables_basic_data_types.cpp
@@ -2,5 +2,13 @@
 
 int main() {
 
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n'; // display
+    std::cout << y << '\n'; // display
+    std::cout << sum << '\n'; // display
+
     return 0;
 }
