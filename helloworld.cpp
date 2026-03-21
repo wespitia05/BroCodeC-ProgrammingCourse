fe8318f10969ef7106e13225237e0e033b1c73e3
william <espiw@farmingdale.edu>
2026-03-21 15:37:12 -0400

    couple of notes

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include <iostream> // functions for basic input/output operations
 
+// begin program by invoking the main function
 int main() {
-    std::cout << "hello world";
+    // std = standard, cout = character output, << = output, endl = endline (flushes output buffer)
+    std::cout << "hello world" << std::endl;
+    std::cout << "i like pizza" << '\n'; // '\n' also works
 
-    return 0;
+    return 0; // exit the program, when reached no problems faced
 }
