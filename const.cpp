e36f9e96f0689eb3be15c5b1c86e6f4417ecd0c2
william <espiw@farmingdale.edu>
2026-03-23 19:23:12 -0400

    first example

--- const.cpp
+++ const.cpp
@@ -5,5 +5,11 @@ int main() {
     // tells the compiler to prevent anything from modifying it
     // (read-only)
 
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm;"
+
     return 0;
 }
