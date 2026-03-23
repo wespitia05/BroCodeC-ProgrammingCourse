6c78ce222e8e8a117aefd7ed9817897871bb2bea
william <espiw@farmingdale.edu>
2026-03-23 19:23:59 -0400

    second example

--- const.cpp
+++ const.cpp
@@ -9,7 +9,11 @@ int main() {
     double radius = 10;
     double circumference = 2 * PI * radius;
 
-    std::cout << circumference << "cm;"
+    std::cout << circumference << "cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     return 0;
 }
