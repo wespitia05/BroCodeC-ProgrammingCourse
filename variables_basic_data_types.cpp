9f6f61a1dbcbed6bd49de88295993404009c455b
william <espiw@farmingdale.edu>
2026-03-23 18:37:59 -0400

    using doubles

--- variables_basic_data_types.cpp
+++ variables_basic_data_types.cpp
@@ -2,6 +2,7 @@
 
 int main() {
 
+    // integer (whole number)
     int x = 5;
     int y = 6;
     int sum = x + y;
@@ -9,6 +10,15 @@ int main() {
     std::cout << x << '\n'; // display
     std::cout << y << '\n'; // display
     std::cout << sum << '\n'; // display
+
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    std::cout << price << '\n'; // display
+    std::cout << gpa << '\n'; // display
+    std::cout << temperature << '\n'; // display
 
     return 0;
 }
