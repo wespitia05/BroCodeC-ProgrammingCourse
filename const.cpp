e3dea97bc21482001464cb3cf627520d7757bfd2
william <espiw@farmingdale.edu>
2026-03-23 19:05:09 -0400

    const setup

---
+++ const.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // the const keyword specifies that a variable's value is constant
+    // tells the compiler to prevent anything from modifying it
+    // (read-only)
+
+    return 0;
+}
