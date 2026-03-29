72969bc115f53f5b2fdeeb2cde75c06b96f63299
william <espiw@farmingdale.edu>
2026-03-29 12:34:45 -0400

    namespace setup

---
+++ namespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // namespace = provides a solution for preventing name conflicts
+    //             in large projects. each entity needs a unique name.
+    //             a namespace allows for identically names entities as 
+    //             long as the namespaces are different
+
+    return 0;
+}
