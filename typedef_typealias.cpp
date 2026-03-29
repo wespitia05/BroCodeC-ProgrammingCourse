634158993fc7796be7feb23456fcd1133c67fede
william <espiw@farmingdale.edu>
2026-03-29 14:40:05 -0400

    typedef/typealias setup

---
+++ typedef_typealias.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // typedef = reserved keyword used to create an additional name
+    //           (alias) for another data type
+    //           new identifier for an existing type
+    //           helps with readability and reduces typos
+
+    return 0;
+}
