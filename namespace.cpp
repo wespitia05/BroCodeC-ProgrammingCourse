54d5d7af3e901c295cd841f731ca2f592e307573
william <espiw@farmingdale.edu>
2026-03-29 14:33:17 -0400

    printing different namespace instances

--- namespace.cpp
+++ namespace.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+namespace first{
+    int x = 1;
+}
+
+namespace second{
+    int x = 2;
+}
+
 int main() {
     // namespace = provides a solution for preventing name conflicts
     //             in large projects. each entity needs a unique name.
     //             a namespace allows for identically names entities as 
     //             long as the namespaces are different
+
+    std::cout << first::x << '\n';
+    std::cout << second::x << '\n';
 
     return 0;
 }
