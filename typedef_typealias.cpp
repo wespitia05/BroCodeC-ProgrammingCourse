d553fd6f838d8fe4a828a099af6b12f32644966a
william <espiw@farmingdale.edu>
2026-03-31 22:01:32 -0400

    typedef with the using keyword

--- typedef_typealias.cpp
+++ typedef_typealias.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
 
-typedef std::string text_t;
-typedef int number_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+// this would work the same as typedef
+using text_t = std::string;
+using number_t = int;
 
 int main() {
     // typedef = reserved keyword used to create an additional name
