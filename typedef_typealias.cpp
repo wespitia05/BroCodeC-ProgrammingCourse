1c1012bbfbe277289226dce4fdf9013cd60681b5
william <espiw@farmingdale.edu>
2026-03-31 21:57:36 -0400

    typedef using a string

--- typedef_typealias.cpp
+++ typedef_typealias.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <vector>
+
+typedef std::string text_t;
 
 int main() {
     // typedef = reserved keyword used to create an additional name
     //           (alias) for another data type
     //           new identifier for an existing type
     //           helps with readability and reduces typos
+
+    text_t firstName = "william";
+    std::cout << firstName << '\n';
 
     return 0;
 }
