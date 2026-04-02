11396ef936b18251eae3c67cda541097df4ce6e4
william <espiw@farmingdale.edu>
2026-04-01 22:48:38 -0400

    type conversion setup

---
+++ type_conversion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // type conversion = conversion a value of one data type to another
+    //                   implicit = automatic
+    //                   explicit = precede value with new data type (int)
+
+    return 0;
+}
