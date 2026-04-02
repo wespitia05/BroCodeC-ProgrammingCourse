3186332d4e26ce6beaddba96de952d619035c31e
william <espiw@farmingdale.edu>
2026-04-01 22:13:41 -0400

    addition

--- arithmetic_operator.cpp
+++ arithmetic_operator.cpp
@@ -4,5 +4,13 @@ int main() {
     // arithmetic operators = return the result of a specific
     //                        arithmetic operation (+ - * /)
 
+    int students = 20;
+
+    // students = students + 1;
+    // students += 1; // does the same thing
+    students++; // does the same thing
+
+    std::cout << students;
+
     return 0;
 }
