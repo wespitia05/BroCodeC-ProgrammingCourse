5c57f49ffd77b3f74065a5b34b91a4c323499501
william <espiw@farmingdale.edu>
2026-04-05 13:10:11 -0400

    example one

--- type_conversion.cpp
+++ type_conversion.cpp
@@ -5,5 +5,9 @@ int main() {
     //                   implicit = automatic
     //                   explicit = precede value with new data type (int)
 
+    double x = (int) 3.14;
+
+    std::cout << x << '\n';
+
     return 0;
 }
