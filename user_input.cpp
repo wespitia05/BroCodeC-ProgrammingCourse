21e5a9abefb596b80e9b3f91f39d8b0c7d3e7d3a
william <espiw@farmingdale.edu>
2026-04-05 13:33:10 -0400

    example three, inputting full name and accounting for whitespaces/newline characters

--- user_input.cpp
+++ user_input.cpp
@@ -6,6 +6,7 @@ int main() {
 
     std::string name;
     int age;
+    std::string fullName;
 
     std::cout << "what is your name?: ";
     std::cin >> name;
@@ -13,8 +14,14 @@ int main() {
     std::cout << "what is your age?: ";
     std::cin >> age;
 
+    std::cout << "what is your full name?: ";
+    // use this method to type characters with spaces
+    // >> std::ws removes any newline characters/whitespaces before user input
+    std::getline(std::cin >> std::ws, fullName);
+
     std::cout << "hello " << name << '\n';
     std::cout << "you are " << age << " years old" << '\n';
+    std::cout << "full name: " << fullName << '\n';
 
     return 0;
 }
