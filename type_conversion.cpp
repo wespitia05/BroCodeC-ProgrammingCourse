15a02590c0bb02fbed871831ef71e9b4fe797a68
william <espiw@farmingdale.edu>
2026-04-05 13:13:22 -0400

    example two

--- type_conversion.cpp
+++ type_conversion.cpp
@@ -6,8 +6,11 @@ int main() {
     //                   explicit = precede value with new data type (int)
 
     double x = (int) 3.14;
+    char y = 100; // implicitly cast 100 to character d using ascii table
 
     std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << (char) 100; // explicitly cast the number to character
 
     return 0;
 }
