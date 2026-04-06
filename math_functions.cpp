2a6a2d1d2ebbfea7717877d0fb822f4face9d952
william <espiw@farmingdale.edu>
2026-04-06 18:33:58 -0400

    power and square root example

--- math_functions.cpp
+++ math_functions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
     // useful math related functions
@@ -8,7 +9,11 @@ int main() {
     double z;
 
     // z = std::max(x, y); // returns greater value
-    z = std::min(x, y); // returns greater value
+    // z = std::min(x, y); // returns greater value
+    // std::cout << z << '\n';
+
+    // z = pow(2, 3); // 2^3
+    z = sqrt(9); // squareroot of 9 = 3
     std::cout << z << '\n';
 
     return 0;
