7992e323d17fd78ec520fd4835b42a0bf1e69f44
william <espiw@farmingdale.edu>
2026-04-06 18:26:41 -0400

    max and min example

--- math_functions.cpp
+++ math_functions.cpp
@@ -3,5 +3,13 @@
 int main() {
     // useful math related functions
 
+    double x = 3;
+    double y = 4;
+    double z;
+
+    // z = std::max(x, y); // returns greater value
+    z = std::min(x, y); // returns greater value
+    std::cout << z << '\n';
+
     return 0;
 }
