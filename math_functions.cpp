a7c2682d170b2158089d80a771c900ebcd98fab1
william <espiw@farmingdale.edu>
2026-04-06 18:48:00 -0400

    absolute value, round, ceil and floor examples

--- math_functions.cpp
+++ math_functions.cpp
@@ -4,8 +4,8 @@
 int main() {
     // useful math related functions
 
-    double x = 3;
-    double y = 4;
+    double x = 3.14;
+    double y = 4.97;
     double z;
 
     // z = std::max(x, y); // returns greater value
@@ -13,7 +13,11 @@ int main() {
     // std::cout << z << '\n';
 
     // z = pow(2, 3); // 2^3
-    z = sqrt(9); // squareroot of 9 = 3
+    // z = sqrt(9); // squareroot of 9 = 3
+    // z = abs(-3); // returns absolute value
+    // z = round(x); // round number in general (up or down)
+    // z = ceil(x); // round number up only
+    z = floor(x); // round number down only
     std::cout << z << '\n';
 
     return 0;
