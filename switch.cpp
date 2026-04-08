2d31bfc8d615826b60788afc1e71340176c943a2
william <espiw@farmingdale.edu>
2026-04-08 18:03:35 -0400

    prompt user to enter the number of the month

--- switch.cpp
+++ switch.cpp
@@ -3,6 +3,9 @@
 int main() {
     // switch = alternative to using many "else if" statements
     //          compare one value against matching cases
+    int month;
+    std::cout << "enter the month (1-12): ";
+    std::cin >> month;
 
     return 0;
 }
