69c960e0df4dc9b2c9e90ff3c5e26f46c8fa3ad1
william <espiw@farmingdale.edu>
2026-04-08 18:45:36 -0400

    using switch cases to display the month we chose

--- switch.cpp
+++ switch.cpp
@@ -7,5 +7,44 @@ int main() {
     std::cout << "enter the month (1-12): ";
     std::cin >> month;
 
+    switch(month) {
+        case 1:
+            std::cout << "it is january";
+            break;
+        case 2:
+            std::cout << "it is february";
+            break;
+        case 3:
+            std::cout << "it is march";
+            break;
+        case 4:
+            std::cout << "it is april";
+            break;
+        case 5:
+            std::cout << "it is may";
+            break;
+        case 6:
+            std::cout << "it is june";
+            break;
+        case 7:
+            std::cout << "it is july";
+            break;
+        case 8:
+            std::cout << "it is august";
+            break;
+        case 9:
+            std::cout << "it is september";
+            break;
+        case 10:
+            std::cout << "it is october";
+            break;
+        case 11:
+            std::cout << "it is november";
+            break;
+        case 12:
+            std::cout << "it is december";
+            break;
+    }
+
     return 0;
 }
