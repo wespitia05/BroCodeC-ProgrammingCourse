e115d2914d23d4ff014eee4352ea696e5d9d27a5
william <espiw@farmingdale.edu>
2026-04-07 20:02:32 -0400

    prompt user to enter value for side a and side b

--- hypotenuse_calculator.cpp
+++ hypotenuse_calculator.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
     // hypotenuse calculator
+
+    double a;
+    double b;
+    double c;
+
+    std::cout << "enter side A: ";
+    std::cin >> a;
+    std::cout << "enter side B: ";
+    std::cin >> b;
 
     return 0;
 }
