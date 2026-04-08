b146bd34007ca62d28baaf16b637fa83013a7697
william <espiw@farmingdale.edu>
2026-04-08 17:57:55 -0400

    basic if/else statement

--- if_statement.cpp
+++ if_statement.cpp
@@ -7,7 +7,14 @@ int main() {
     int age;
 
     std::cout << "enter your age: ";
-    std::cin << age;
+    std::cin >> age;
+
+    if (age >= 18) {
+        std::cout << "welcome to the site!";
+    }
+    else {
+        std::cout << "you are not old enough to enter!";
+    }
 
     return 0;
 }
