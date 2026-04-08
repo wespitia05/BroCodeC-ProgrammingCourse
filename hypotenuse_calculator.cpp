33f5a20c8d282285e845219490505556dab47d6a
william <espiw@farmingdale.edu>
2026-04-07 20:43:19 -0400

    calculation

--- hypotenuse_calculator.cpp
+++ hypotenuse_calculator.cpp
@@ -13,5 +13,11 @@ int main() {
     std::cout << "enter side B: ";
     std::cin >> b;
 
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+
+    std::cout << "side C: " << c;
+
     return 0;
 }
