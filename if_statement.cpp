4da1fd8965ec1f0fbdfe02a1ca1795dde6528088
william <espiw@farmingdale.edu>
2026-04-08 17:56:06 -0400

    prompt user for their age

--- if_statement.cpp
+++ if_statement.cpp
@@ -4,5 +4,10 @@ int main() {
     // if statements = do something if a condition is true
     //                 if not, then don't do it
 
+    int age;
+
+    std::cout << "enter your age: ";
+    std::cin << age;
+
     return 0;
 }
