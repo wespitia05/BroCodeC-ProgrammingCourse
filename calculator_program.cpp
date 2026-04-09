56e07a76d5f75160122c8eb4b4b3a60ff1d10249
william <espiw@farmingdale.edu>
2026-04-09 18:31:37 -0400

    declare our variables and user prompts

--- calculator_program.cpp
+++ calculator_program.cpp
@@ -3,5 +3,23 @@
 int main() {
     // calculator program
 
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "********** CALCULATOR **********\n";
+
+    std::cout << "enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "enter number 1: ";
+    std::cin >> num1;
+
+    std::cout << "enter number 2: ";
+    std::cin >> num2;
+
+    std::cout << "********************************";
+
     return 0;
 }
