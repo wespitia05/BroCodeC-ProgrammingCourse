34bbe4c1b763f395246fa17b0ca95c49c48b191c
william <espiw@farmingdale.edu>
2026-04-08 22:13:54 -0400

    switch cases for character input

--- switch.cpp
+++ switch.cpp
@@ -52,5 +52,25 @@ int main() {
     std::cout << "what letter grade?: ";
     std::cin >> grade;
 
+    switch(grade) {
+        case 'A':
+            std::cout << "you did great!";
+            break;
+        case 'B':
+            std::cout << "you did good";
+            break;
+        case 'C':
+            std::cout << "you did okay";
+            break;
+        case 'D':
+            std::cout << "you did not do good";
+            break;
+        case 'F':
+            std::cout << "you failed";
+            break;
+        default:
+            std::cout << "please only enter in a letter grade (A-F)";
+    }
+
     return 0;
 }
