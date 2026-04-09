2e109b0eead671ac8307b43344dcd57e1e918fae
william <espiw@farmingdale.edu>
2026-04-09 18:36:15 -0400

    example of an if/else statement

--- ternary_operator.cpp
+++ ternary_operator.cpp
@@ -4,5 +4,14 @@ int main() {
     // ternary operator ? = replacement to an if/else statement
     // condition ? expression1 : expression2;
 
+    int grade = 75;
+
+    if (grade >= 60) {
+        std::cout << "you pass";
+    }
+    else {
+        std::cout << "you fail";
+    }
+
     return 0;
 }
