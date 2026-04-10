61b1a4340a6147ba4340cc33ea8883c4b8573a7a
william <espiw@farmingdale.edu>
2026-04-10 18:38:50 -0400

    logical operators setup

---
+++ logical_operator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // && = check if two conditions are true
+    // || check if at least one of the two conditions is true
+    // !  = reverses the logical state of its operand
+
+    return 0;
+}
