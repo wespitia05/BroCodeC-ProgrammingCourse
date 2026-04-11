24a700434cf61a7db8d3d6616b48917ad561eab1
william <espiw@farmingdale.edu>
2026-04-11 16:31:09 -0400

    functionality

--- temp_conversion.cpp
+++ temp_conversion.cpp
@@ -14,6 +14,24 @@ int main() {
     std::cout << "What unit would you like to convert to?: ";
     std::cin >> unit;
 
+    if(unit == 'F' || unit == 'f') {
+        std::cout << "Enter the temperature in Celsius: ";
+        std::cin >> temp;
+
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "Temperature is: " << temp << "F\n";
+    }
+    else if(unit == 'C' || unit == 'c') {
+        std::cout << "Enter the temperature in Fahrenheit: ";
+        std::cin >> temp;
+
+        temp = (temp - 32.0) / 1.8;
+        std::cout << "Temperature is: " << temp << "C\n";
+    }
+    else {
+        std::cout << "Please enter in only C or F\n";
+    }
+
     std::cout << "********************************************";
 
     return 0;
