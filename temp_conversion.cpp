27c90225d7a12960c51cb714d4eebda46b20974f
william <espiw@farmingdale.edu>
2026-04-11 16:28:36 -0400

    prompt for user to enter the unit we would like to convert to

--- temp_conversion.cpp
+++ temp_conversion.cpp
@@ -2,6 +2,19 @@
 
 int main() {
     // temperature conversion program
-    
+
+    double temp;
+    char unit;
+
+    std::cout << "********** TEMPERATURE CONVERSION **********\n";
+
+    std::cout << "F = Fahrenheit\n";
+    std::cout << "C = Celsius\n";
+
+    std::cout << "What unit would you like to convert to?: ";
+    std::cin >> unit;
+
+    std::cout << "********************************************";
+
     return 0;
 }
