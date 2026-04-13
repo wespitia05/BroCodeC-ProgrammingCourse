441ab23c0e0fdb616a76eff90f9f77546af3c99c
william <espiw@farmingdale.edu>
2026-04-13 18:00:36 -0400

    length method example

--- string_methods.cpp
+++ string_methods.cpp
@@ -3,5 +3,18 @@
 int main() {
     // string methods
 
+    std::string name;
+
+    std::cout << "enter your name: ";
+    std::getline(std::cin, name);
+
+    // built in length method for strings
+    if(name.length() > 12) {
+        std::cout << "your name can't be over 12 characters";
+    }
+    else {
+        std::cout << "welcome " << name;
+    }
+
     return 0;
 }
