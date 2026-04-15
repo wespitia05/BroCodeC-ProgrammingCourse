ba4aea82186e12cc09608156e9f0a4df27eaadcf
william <espiw@farmingdale.edu>
2026-04-15 18:33:38 -0400

    while loop example

--- while_loops.cpp
+++ while_loops.cpp
@@ -3,5 +3,14 @@
 int main() {
     // while loop = run some code while a condition is true
 
+    std::string name;
+
+    while (name.empty()) {
+        std::cout << "enter your name: ";
+        std::getline(std::cin, name);
+    }
+
+    std::cout << "hello " << name;
+
     return 0;
 }
