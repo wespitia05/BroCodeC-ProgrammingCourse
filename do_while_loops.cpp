39cd3bba29f9d9f26e90f2a71bbc7cc79121abbb
william <espiw@farmingdale.edu>
2026-04-17 20:17:30 -0400

    do while loop example

--- do_while_loops.cpp
+++ do_while_loops.cpp
@@ -4,5 +4,14 @@ int main() {
     // do while loop = do some block of code first,
     //                 THEN repeat again if condition is true
 
+    int number;
+
+    do {
+        std::cout << "enter a positive number: ";
+        std::cin >> number;
+    } while (number < 0);
+
+    std::cout << "the number is: " << number;
+
     return 0;
 }
