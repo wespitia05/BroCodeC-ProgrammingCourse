85d349529dc94fc811eecd4f329ad43c9a6a1c1f
william <espiw@farmingdale.edu>
2026-04-19 15:02:42 -0400

    break example

--- break_continue.cpp
+++ break_continue.cpp
@@ -4,5 +4,14 @@ int main() {
     // break = break out of a loop
     // continue = skip current iteration
 
+    // loop through each item from 1-20 and print each number
+    for (int i = 1; i <= 20; i++) {
+        // if the number = 13, end the for loop
+        if (i == 13) {
+            break;
+        }
+        std::cout << i << '\n';
+    }
+
     return 0;
 }
