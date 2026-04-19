69715c37af6551ae40bada1805f541f624287d74
william <espiw@farmingdale.edu>
2026-04-19 14:45:36 -0400

    printing statement 3 times

--- for_loops.cpp
+++ for_loops.cpp
@@ -3,6 +3,11 @@
 int main() {
     // for loop = loop that will execute a block of code a specified
     //            number of times
-    
+
+    // prints statement 3 times
+    for (int i = 1; i <= 3; i++) {
+        std::cout << "HAPPY NEW YEAR!\n";
+    }
+
     return 0;
 }
