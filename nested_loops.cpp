53af80c346f6e0f2ae6089ef0a849c834408447c
william <espiw@farmingdale.edu>
2026-04-21 17:50:38 -0400

    functionality

--- nested_loops.cpp
+++ nested_loops.cpp
@@ -27,5 +27,12 @@ int main() {
     std::cout << "enter a symbol to use: ";
     std::cin >> symbol;
 
+    for(int i = 1; i <= rows; i++) {
+        for(int j = 1; j <= columns; j++) {
+            std::cout << symbol;
+        }
+        std::cout << '\n';
+    }
+
     return 0;
 }
