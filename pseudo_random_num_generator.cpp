13edc3ae3e4911f4998186cb1af33d87f7e907f9
william <espiw@farmingdale.edu>
2026-04-21 17:58:43 -0400

    generating random numbers, 1-6 1-10 and 1-20

--- pseudo_random_num_generator.cpp
+++ pseudo_random_num_generator.cpp
@@ -3,5 +3,16 @@
 int main() {
     // pseudo-random = NOT truly random (but close)
 
+    srand(time(NULL));
+
+    // gives us a random number from 1-6
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 10) + 1;
+    int num3 = (rand() % 20) + 1;
+
+    std::cout << num1;
+    std::cout << num2;
+    std::cout << num3;
+
     return 0;
 }
