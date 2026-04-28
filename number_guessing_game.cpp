98b12f30208b5d367b56ddb9e2dd2942f5621aa3
william <espiw@farmingdale.edu>
2026-04-28 17:34:24 -0400

    guessing functionality

--- number_guessing_game.cpp
+++ number_guessing_game.cpp
@@ -11,5 +11,25 @@ int main() {
     srand(time(NULL));
     num = (rand() % 100) + 1;
 
+    std::cout << "********** NUMBER GUESSING GAME **********\n";
+
+    do {
+        std::cout << "enter a guess between (1-100): ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num) {
+            std::cout << "too high\n";
+        }
+        else if(guess < num) {
+            std::cout << "too low\n";
+        }
+        else {
+            std::cout << "correct! # of tries: " << tries << '\n';
+        }
+    } while (guess != num);
+
+    std::cout << "******************************************\n";
+
     return 0;
 }
