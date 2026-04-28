e765b90949a21375100cfac55d3e215364f5b464
william <espiw@farmingdale.edu>
2026-04-28 18:01:43 -0400

    different variable type as a second parameter

--- user_defined_functions.cpp
+++ user_defined_functions.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 
-void happyBirthday(std::string name) {
+void happyBirthday(std::string name, int age) {
     std::cout << "happy birthday to " << name << "!\n";
     std::cout << "happy birthday to " << name << "!\n";
     std::cout << "happy birthday dear " << name << "!\n";
     std::cout << "happy birthday to " << name << "!\n";
+    std::cout << "you are " << age << " years old!\n";
 }
 
 int main() {
     // function = a block of reusable code
 
     std::string name = "william";
+    int age = 28;
 
-    happyBirthday(name);
-    happyBirthday(name);
+    happyBirthday(name, age);
+    happyBirthday(name, age);
 
     return 0;
 }
