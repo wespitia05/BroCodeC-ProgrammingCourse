e97918ef1aaa37dcac642fc2a55583843059750d
william <espiw@farmingdale.edu>
2026-04-28 17:57:07 -0400

    calling function twice

--- user_defined_functions.cpp
+++ user_defined_functions.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+void happyBirthday() {
+    std::cout << "happy birthday to you!\n";
+    std::cout << "happy birthday to you!\n";
+    std::cout << "happy birthday dear you!\n";
+    std::cout << "happy birthday to you!\n\n";
+}
+
 int main() {
     // function = a block of reusable code
+
+    happyBirthday();
+    happyBirthday();
 
     return 0;
 }
